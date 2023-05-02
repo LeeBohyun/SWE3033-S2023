91fca319a7ec0fd1fa9ebccef1905bf309b9e2b0
Lee Bohyun <55489991+LeeBohyun@users.noreply.github.com>
2023-05-02 11:14:29 +0900

    Update run_dbbench.sh

--- week9/run_dbbench.sh
+++ week9/run_dbbench.sh
@@ -5,13 +5,13 @@
         -db="/home/lbh/rocksdb-data" \
         -use_direct_io_for_flush_and_compaction=true \
         -use_direct_reads=true \
-        -compaction_style=0 \ 
+        -compaction_style=0\
         -target_file_size_base=2097152 \
         -write_buffer_size=2097152 \
         -max_bytes_for_level_base=33554432 \
         -max_bytes_for_level_multiplier=3 \
         -num=10000000 \
-        --key_size=16 --value_size=240 \ 
+        -key_size=16 -value_size=240\
         -statistics \
         -stats_dump_period_sec=30 \
         -stats_interval_seconds=10 2>&1 | tee result.txt
@@ -21,13 +21,13 @@
         -db="/home/lbh/rocksdb-data" \
         -use_direct_io_for_flush_and_compaction=true \
         -use_direct_reads=true \
-        -compaction_style=1 \ 
+        -compaction_style=1\
         -target_file_size_base=2097152 \
         -write_buffer_size=2097152 \
         -max_bytes_for_level_base=33554432 \
         -max_bytes_for_level_multiplier=3 \
         -num=10000000 \
-        --key_size=16 --value_size=240 \ 
+        -key_size=16 -value_size=240\
         -statistics \
         -stats_dump_period_sec=30 \
         -stats_interval_seconds=10 2>&1 | tee result1.txt
@@ -37,13 +37,13 @@
         -db="/home/lbh/rocksdb-data" \
         -use_direct_io_for_flush_and_compaction=true \
         -use_direct_reads=true \
-        -compaction_style=0 \ 
+        -compaction_style=0\
         -target_file_size_base=2097152 \
         -write_buffer_size=2097152 \
         -max_bytes_for_level_base=33554432 \
         -max_bytes_for_level_multiplier=3 \
         -num=10000000 \
-        --key_size=16 --value_size=240 \ 
+        -key_size=16 -value_size=240\
         -statistics \
         -stats_dump_period_sec=30 \
         -stats_interval_seconds=10 2>&1 | tee result2.txt
